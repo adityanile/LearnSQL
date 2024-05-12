fcb0c3d70fb540fabfa0dd5c8da893d3e90ba0bf
Aditya Nile <adityanile@gmail.com>
2024-05-12 13:58:37 +0530

    Fetch Questions Data From Server

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection.Emit;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class MainManager : MonoBehaviour
 {
@@ -6,6 +12,7 @@ public class MainManager : MonoBehaviour
     private MainData mainData;
 
     private LocalStorageManager storageManager;
+    private string apiUrl = "https://learn-sql.vercel.app/getMeQuestions";
 
     private void Awake()
     {
@@ -15,6 +22,8 @@ public class MainManager : MonoBehaviour
     void Start()
     {
         Init();
+
+        FetchQuestionData("Theory", 1);
     }
 
     private void Init()
@@ -38,12 +47,87 @@ public class MainManager : MonoBehaviour
             }
         }
 
-
-        // Test
-        mainData.categories[0].levels[0].questions = new QuestionData[10];
-
     }
 
+    // This function will fetch question data from server and save it in mainData
+    public void FetchQuestionData(string tag, int level)
+    {
+        Payload payload = new Payload();
+        payload.tag = tag;
+        payload.level = level;
+
+        string json = JsonUtility.ToJson(payload);  
+
+        StartCoroutine(GetQuestionsAPI(json));
+    }
+
+    IEnumerator GetQuestionsAPI(string payload)
+    {
+        using(UnityWebRequest req = UnityWebRequest.Post(apiUrl, payload, "application/json"))
+        {
+            yield return req.SendWebRequest();
+
+            if (req.result != UnityWebRequest.Result.ConnectionError)
+            {
+                string rcv = req.downloadHandler.text;
+
+                // Parsing the Received JSON
+                Received received = new Received();
+                received = JsonUtility.FromJson<Received>(rcv);
+
+                // Set main data to these received Data
+                
+                if(received.ststus == "success")
+                {
+                    if (received.ret.Length > 0)
+                    {
+                        // Parsing the received Tag
+                        Tag st;
+                        if (Enum.TryParse(received.ret[0].tag, out st))
+                        {
+                            int index = received.ret[0].level;
+                            mainData.categories[Convert.ToInt32(st)].levels[index].questions = received.ret;
+
+                            // Call function from here to show question for a session
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log("No Questions Found on Server of Such Type");
+                    }
+                }
+                else
+                {
+                    Debug.Log("Error Fetching Data");
+                }
+                
+
+
+            }
+            else
+            {
+                Debug.Log("Error Connecting Server");
+            }
+
+        }
+    }
+
+    // To make payload json
+    [System.Serializable]
+    class Payload
+    {
+        public string tag;
+        public int level;
+    }
+
+    [System.Serializable]
+    class Received
+    {
+        public string ststus;
+        public QuestionData[] ret;
+    }
+
+    // Class To Manager Data after fetching Question
     [System.Serializable]
     class MainData
     {
@@ -67,14 +151,27 @@ public class MainManager : MonoBehaviour
     }
 
     [System.Serializable]
-    class QuestionData
+    public class QuestionData
     {
+        public int id;
         public string que;
         public string op1;
         public string op2;
         public string op3;
         public string op4;
         public string correct;
+
+        // To handle the rcv also
+        public string tag;
+        public int level;
     }
 
+}
+
+[System.Serializable]
+public enum Tag
+{
+    Theory,
+    Coding,
+    Interview
 }
