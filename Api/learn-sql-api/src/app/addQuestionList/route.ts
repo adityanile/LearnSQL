20bc117cbce93e99e42538304f619fc73163ec98
Aditya Nile <adityanile@gmail.com>
2024-05-14 20:44:13 +0530

    addQuestionList

--- Api/learn-sql-api/src/app/addQuestionList/route.ts
+++ Api/learn-sql-api/src/app/addQuestionList/route.ts
@@ -9,12 +9,12 @@ export async function POST(req: Request) {
   const que: dat = await req.json();
   let ret;
 
-  for (let i = 0; i < que.ques.length; i++) {
-    try {
-      ret = await prisma.questionData.create({ data: que.ques[i] });
-    } catch (e) {
-      return NextResponse.json({ status: "fail", e });
-    }
+  try {
+    ret = await prisma.questionData.createMany({
+      data: que.ques,
+    });
+  } catch (e) {
+    return NextResponse.json({ status: "fail", e });
   }
 
   return NextResponse.json({ msg: "Done", Inserted: que.ques.length });
